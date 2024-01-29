7ac92a1e883a8c53314db305d2301dd24dbff18f
Niels Dommerholt <nielsutrecht@gmail.com>
2024-01-29 11:03:35 +0100

    Remove Apache AGE PoC

--- apache-age/src/main/java/com/nibado/examples/spring/age/repository/WaypointEntity.java
+++
@@ -1,8 +0,0 @@
-package com.nibado.examples.spring.age.repository;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
-@Table("waypoint")
-public record WaypointEntity(@Id String symbol, String systemSymbol, String type, int x, int y) {
-}
